03bbfddd0334ed2ba14f38313b3241b8225dc814
neutricoindia <166170548+neutricoindia@users.noreply.github.com>
2024-07-16 04:54:48 +0530

    updated

--- src/script.js
+++ src/script.js
@@ -2,9 +2,13 @@ import MouseEventSDK from "./MouseEventSDK";
 
 const sdk = new MouseEventSDK(200, 1000);
 const textarea = document.getElementById("text-area");
-document.getElementById("start-btn").addEventListener("click", sdk.start);
-document.getElementById("stop-btn").addEventListener("click", sdk.stop);
-document.getElementById("get-data-btn").addEventListener("click", () => {
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+const getDataBtn = document.getElementById("get-data-btn");
+
+startBtn.addEventListener("click", sdk.start);
+stopBtn.addEventListener("click", sdk.stop);
+getDataBtn.addEventListener("click", () => {
   textarea.value = JSON.stringify(sdk.getData());
   // console.log(sdk.getData());
 });
